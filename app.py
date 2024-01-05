f0763fdd4fa8ce516247e75cc44baa0f253c1a7b
Jazmine James <jazminjames92@gmail.com>
2024-01-05 12:19:21 -0800

    imported load_dotenv.

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, request, flash, redirect, session, g, 
 from sqlalchemy.exc import IntegrityError
 from forms import UserAddForm, LoginForm, MessageForm
 from models import db, connect_db, User, Message, Likes, Follows
-
+from dotenv import load_dotenv
 CURR_USER_KEY = "curr_user"
 
 app = Flask(__name__)
@@ -12,11 +12,13 @@ app = Flask(__name__)
 # Get DB_URI from environ variable (useful for production/testing) or,
 # if not set there, use development local db.
 load_dotenv()
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+database_url = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+secret_key = os.getenv('SECRET_KEY')
+app.config['SECRET_KEY'] = secret_key
 """ toolbar = DebugToolbarExtension(app) """
 
 connect_db(app)
